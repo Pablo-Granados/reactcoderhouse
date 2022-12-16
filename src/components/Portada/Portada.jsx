97a959d9d232c46fb09c4c5f286ecd6955b23a0a
Pablo-Granados <pablogranados22@hotmail.com>
2022-12-16 19:22:45 -0300

    correcciones 2

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -5,8 +5,10 @@ function Portada({titulo, subtitulo, img, alt}) {
 
     return (
         <div className="fondo">
+            <div className="fondoTxt">
             <h2 className="titulo">{titulo}</h2>
             <h4 className="subtitulo">{subtitulo}</h4>
+            </div>
             <img className="fondoIMG"
                 src={img}
                 alt={alt}
