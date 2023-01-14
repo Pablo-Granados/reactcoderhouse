ddc619b3de3dfb28353cc85486696999e0e32cac
Pablo-Granados <pablogranados22@hotmail.com>
2023-01-14 20:50:42 -0300

    Probando nueva rama, trabajando desde otra pc

--- src/components/InicioAPP/InicioAPP.jsx
+++ src/components/InicioAPP/InicioAPP.jsx
@@ -7,7 +7,7 @@ function InicioAPP() {
         <div className="home">
             <Portada 
             titulo= "La coctelera"
-            subtitulo="Las mejores recetas para tus tragos"
+            subtitulo="Las mejores recetas e insumos para tus tragos"
             img="https://images.losandes.com.ar/resizer/5QdOTebiuhsTZRMbHdAndLbvqRk=/980x640/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/R5B764D2LRB4VGDN7RZVC4SLLE.jpg"
             alt="Cocteleria"
             />
